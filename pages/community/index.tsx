41168d275bc9c8f97a7fe57f5ef42d38c9cb5ee2
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-10-22 13:42:39 +0900

    feat: integration header and footer via Layouts

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,7 +1,8 @@
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { NextPage } from "next";
 import { useState } from "react";
 
-const Community = () => {
-  console.log("COMMUNITY COMPONENT - PAGES ROUTING");
+const Community: NextPage = () => {
   const [title, setTitle] = useState<string>("hello");
   return (
     <div>
@@ -13,4 +14,4 @@ const Community = () => {
   );
 };
 
-export default Community;
+export default withLayoutBasic(Community);
--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,6 +1,13 @@
-const PropertyList = () => {
-  console.log("Property List - PAGES ROUTING");
-  return <div> Property LIST </div>;
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { Container, Stack } from "@mui/material";
+import { NextPage } from "next";
+
+const PropertyList: NextPage = () => {
+  return (
+    <>
+      <Container>PROPERTY LIST</Container>
+    </>
+  );
 };
 
-export default PropertyList;
+export default withLayoutBasic(PropertyList);
