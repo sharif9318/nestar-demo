f442ec06432088ebea513c71055438b715433d25
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-10-23 10:50:58 +0900

    feat: publish main layout component

--- libs/components/layout/LayoutHome.tsx
+++ libs/components/layout/LayoutHome.tsx
@@ -1,19 +1,33 @@
 import { Stack } from "@mui/material";
 import Head from "next/head";
+import Top from "../Top";
+import Footer from "../Footer";
+import HeaderFilter from "../homepage/headerFilter";
 
 const withLayoutMain = (Component: any) => {
   return (props: any) => {
     return (
       <>
         <Head>
-          <title>Nestar</title>
+          <title>Mestar</title>
         </Head>
         <Stack id="pc-wrap">
-          <Stack sx={{ background: "#81c784" }}>Header Home</Stack>
-          <Stack id={"main"}>
+          <Stack id="top">
+            <Top />
+          </Stack>
+          <Stack className="header-main">
+            <Stack className="container">
+              <HeaderFilter />
+            </Stack>
+          </Stack>
+
+          <Stack id="main">
             <Component {...props} />
           </Stack>
-          <Stack sx={{ background: "#a1887f" }}>Footer</Stack>
+
+          <Stack id="footer">
+            <Footer />
+          </Stack>
         </Stack>
       </>
     );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,18 @@
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
-import { Box, Container, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <Stack>
-      <Stack flexDirection="column">
-        <Stack>
-          <Stack className="container">Popular Properties</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Top Agents</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Top Properties</Stack>
-        </Stack>
+    <Stack className="home-page">
+      <Stack>
+        <Stack className="container">Popular Properties</Stack>
+      </Stack>
+      <Stack>
+        <Stack className="container">Top Agents</Stack>
+      </Stack>
+      <Stack>
+        <Stack className="container">Top Properties</Stack>
       </Stack>
     </Stack>
   );
