d67e2b4f6ed71dcfa84349ae7d6df01ff1a25014
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-10-26 09:48:45 +0900

    feat: develop apollo client connection

---
+++ apollo/client.ts
@@ -0,0 +1,13 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+
+//No Subscription required for develop process
+const httpLink = createHttpLink({
+  uri: "http://localhost:3007/graphql",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+export default client;
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,32 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import Advertisement from "@/libs/components/homepage/Advertisement";
 import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+import { useQuery } from "@apollo/client";
+import { GET_PROPERTIES } from "@/apollo/user/query";
 
 const Home: NextPage = () => {
   const device = useDeviceDetect();
+
+  const {
+    loading: getPropertiesLoading,
+    error: getPropertiesError,
+    data: getPropertiesData,
+    refetch: getPropertiesRefetch,
+  } = useQuery(GET_PROPERTIES, {
+    fetchPolicy: "network-only",
+    variables: {
+      input: {
+        page: 1,
+        limit: 5,
+        sort: "propertyRank",
+        direction: "DESC",
+        search: {},
+      },
+    },
+  });
+
+  console.log("getPropertiesData", getPropertiesData);
+
   if (device === "mobile") {
     return <Stack> HOMEPAGE MOBILE</Stack>;
   } else {
